a76c6c685f877d5f2f05651b5673b6569438294a
Laura Young <0866297@hr.nl>
2018-03-28 12:59:54 +0200

    basic layout
    layout en possible logo name

---
+++ WindowsFormsApp1/MapRender/MusicPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.MapRender
+{
+    public class MusicPoint
+    {
+
+        private Label tlabel;
+        private PictureBox box;
+
+        public MusicPoint( MusicIcon ico, Point xypoint, PictureBox pbParent, ImageList images)
+        {
+
+            box = new PictureBox();
+            box.Parent = pbParent;
+            box.Image = images.Images[(int)ico];
+            xypoint.X = xypoint.X + 10;
+            xypoint.Y = xypoint.Y + 10;
+            box.Location = xypoint;
+            box.BackColor = Color.Transparent;
+            box.BringToFront();
+            box.Show();
+
+        }
+    }
+}
